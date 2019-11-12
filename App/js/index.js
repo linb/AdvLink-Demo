c073fb5a541cfd970bb0edacb0557e6721270ebd
Jack Lee <linb.net@gmail.com>
2019-11-12 17:12:40 -0500

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,53 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.Link")
+                .setHost(host,"xui_ui_link1")
+                .setLeft("7.111111111111111em")
+                .setTop("4.444444444444445em")
+                .setWidth("26.755555555555556em")
+                .setHeight("1.2444444444444445em")
+                .setCaption("Click the link to show website in below div")
+                .setHref("https://crossui.com")
+                .onClick({
+                    "return":"{false}",
+                    "actions":[
+                        {
+                            "desc":"Action 1",
+                            "type":"control",
+                            "target":"xui_ui_div17",
+                            "args":[
+                                { },
+                                {
+                                    "iframeAutoLoad":"{args[0].boxing().getHref()}"
+                                }
+                            ],
+                            "method":"setProperties",
+                            "event":1
+                        }
+                    ]
+                })
+            );
+            
+            append(
+                xui.create("xui.UI.Div")
+                .setHost(host,"xui_ui_div17")
+                .setLeft("4.444444444444445em")
+                .setTop("12.444444444444445em")
+                .setWidth("39.37777777777778em")
+                .setHeight("17.155555555555555em")
+                .setCustomStyle({
+                    "KEY":{
+                        "border-top":"solid 1px #000000",
+                        "border-right":"solid 1px #000000",
+                        "border-bottom":"solid 1px #000000",
+                        "border-left":"solid 1px #000000"
+                    }
+                })
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
